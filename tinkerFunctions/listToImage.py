d6a953a1c297dc72b2ef3378c1e659ee8bb30689
icarus <ellishogan95@gmail.com>
2019-09-20 18:56:09 -0400

    listToImage still needs to be checked to make sure it works

--- tinkerFunctions/listToImage.py
+++ tinkerFunctions/listToImage.py
@@ -3,10 +3,11 @@ import pandas as pd
 import requests
 import re
 links = [i for i in pd.read_csv(sys.argv[1])]
+place = sys.argv[2] if len(sys.argv) >= 3 else "./saveFile"
 for i in range(len(links)):
     r = requests.get(links[i])
     x = re.search(r'\.[psj][pvn].*g', links[i])
     name = f'{i}' + x.group()
-    print(name)
-    with open(f'./saveFile/{name}', 'wb') as f:
+    print("saving " + name)
+    with open(f'{place}/{name}', 'wb') as f:
         f.write(r.content)
