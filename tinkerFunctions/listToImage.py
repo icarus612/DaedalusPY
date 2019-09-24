67d7774c512a870a4bf186d8f7ef4f5260d173bc
icarus <ellishogan95@gmail.com>
2019-09-24 10:45:59 -0400

    minor fixes

--- tinkerFunctions/listToImage.py
+++ tinkerFunctions/listToImage.py
@@ -2,12 +2,18 @@ import sys
 import pandas as pd
 import requests
 import re
+import os
 links = [i for i in pd.read_csv(sys.argv[1])]
 place = sys.argv[2] if len(sys.argv) >= 3 else "./saveFile"
+try:
+    os.mkdir(place)
+except FileExistsError:
+    pass
 for i in range(len(links)):
-    r = requests.get(links[i])
+    req = requests.get(links[i])
     x = re.search(r'\.[psj][pvn].*g', links[i])
     name = f'{i}' + x.group()
     print("saving " + name)
-    with open(f'{place}/{name}', 'wb') as f:
-        f.write(r.content)
+    with open(f'{place}/{name}', 'wb') as file:
+        file.write(req.content)
+
